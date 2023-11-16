ee30c9c1c91e9767a263de0894280f07780f7582
JJohns2300 <jtowin@comcast.net>
2023-11-15 23:56:49 -0600

    server.js connected to database port

--- api/server.js
+++ api/server.js
@@ -10,8 +10,8 @@ app.use(cors());
 mongoose.connect("mongodb://localhost:27017/milestone2thejj", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+})  //CALLBACK FUNCTION TO CONNECT TO DATABASE//
     .then(() => console.log("Connect to Datbase"))
     .catch(console.error);
 
-app.listen(3001);
+app.listen(3001, () => console.log(("Server opened on port 3001")));
