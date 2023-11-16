bfff841fd2b6e975ab6b797e6155b6696eb4b45e
JJohns2300 <jtowin@comcast.net>
2023-11-16 10:11:03 -0600

    created new user schema

--- api/models/todo.js
+++ api/models/todo.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+//NEW USER TO-DO SCHEMA FUNCTION//
 const TodoSchema = new Schema({
-    text: {
-        type: string
+    Text: {
+        type: String,
         required: true
+    }, 
+    complete: {
+        type: Boolean, 
+        default: false
+    },
+    timestamp: {
+        type: String, 
+        default: Date.now()
     }
-    complete,
-    timestamp
 })
