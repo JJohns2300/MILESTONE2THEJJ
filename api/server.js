1af65726a588954b4a3e26c0c8e0db5795a02489
JJohns2300 <jtowin@comcast.net>
2023-11-15 23:24:43 -0600

    created mongoose database

--- api/server.js
+++ api/server.js
@@ -6,3 +6,12 @@ const app = express ('');
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/milestone2thejj", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connect to Datbase"))
+    .catch(console.error);
+
+app.listen(3001);
